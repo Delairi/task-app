498c19e983935f853f89e2c3f6082c75126303fc
delairi <adriansw20@hotmail.com>
2025-07-07 01:51:23 -0500

    add header

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Card } from './components/card/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [Card],
+  imports: [Card, CommonModule],
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+
+  tasks = new Array(12).fill(0)
 
 }
